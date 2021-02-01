9ed428f048a283f636e6c30a334d30cbb27e8462
Markiiiiiiii <bs255200@163.com>
2021-02-01 13:17:25 +0800

    add

--- loganget/kafka/sarama.go
+++ loganget/kafka/sarama.go
@@ -7,6 +7,29 @@ import (
 )
 
 func main() {
-	config := sarama.NewConfig()
-	fmt.Println(config)
+	config := sarama.NewConfig()                              //新建一个配置
+	config.Producer.RequiredAcks = sarama.WaitForAll          //发送完数据后需要leader和follow都确认ack
+	config.Producer.Partitioner = sarama.NewRandomPartitioner //新选出一个partition
+	config.Producer.Return.Successes = true                   //成功交付了消息将在sucssce channel 返回一个ture
+
+	//构建一个消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "web_log"
+	msg.Value = sarama.StringEncoder("this is a test log")
+
+	//连接kafka
+	clinet, err := sarama.NewSyncProducer([]string{"192.168.1.102:9092"}, config)
+	if err != nil {
+		fmt.Println("producer closed,err:", err)
+		return
+	}
+	defer clinet.Close()
+
+	//发送消息
+	pid, offset, err := clinet.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send msg faild,err:", err)
+		return
+	}
+	fmt.Printf("pid:%v,offset:%v\n", pid, offset)
 }
